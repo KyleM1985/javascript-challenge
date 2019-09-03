9f5d830d00e4cc02efb26706e0fc6eeab87bfe87
Kyle Moretti <gamewhiz7879@gmail.com>
2019-09-03 13:43:26 -0700

    Updated

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+// from data.js
+var tableData = data;
+
+// function to display UFO sightings
+console.log(tableData);
+
+// select tbody Html element
+var tbody = d3.select("tbody");
+
+// send data from data.js to console.log
+console.log(data);
+
+data.forEach(function(ufoSighting) {
+  console.log(ufoSighting);
+  
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+});
+
+// reference to button with id set to 'filter-btn'
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+  
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+    
+  //create variables for user input
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  
+  // filter data based on user input
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  
+  console.log(filteredData);
+  
+});
